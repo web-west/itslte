9235dda4604069d9decf3a00e9411c35884886a9
Fomin <fomvasss@gmail.com>
2019-03-20 23:28:43 +0200

    [*] добавил пример поля x-editable

---
+++ src/includes/field-x-editable.php
@@ -0,0 +1,10 @@
+<a href="#"
+   class="field-x-editable"
+   data-value="Ваше значение"
+   data-type="text"
+   data-name="Название"
+   data-url="./save.php"
+   data-pk="1"
+> Значение по умолчанию
+</a>
+<!-- https://vitalets.github.io/x-editable/ -->
